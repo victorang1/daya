3d894c2b2f7f4874634c7fc2accab240f28cfac5
victor <victor@binus.edu>
2021-05-08 22:25:36 +0700

    add button listener

--- app/src/main/java/bangkit/daya/app/landing/LandingFragment.kt
+++ app/src/main/java/bangkit/daya/app/landing/LandingFragment.kt
@@ -1,10 +1,12 @@
 package bangkit.daya.app.landing
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import bangkit.daya.R
 import bangkit.daya.databinding.FragmentLandingBinding
 import bangkit.daya.model.LandingItem
@@ -13,7 +15,7 @@ import bangkit.daya.util.DepthTransformer
 /**
  * Created by victor on 26-Apr-21 7:24 PM.
  */
-class LandingFragment : Fragment() {
+class LandingFragment : Fragment(), View.OnClickListener {
 
     private lateinit var binding: FragmentLandingBinding
     private val mAdapter: LandingAdapter by lazy { LandingAdapter(requireContext()) }
@@ -31,6 +33,15 @@ class LandingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initData()
+        binding.btnLogin.setOnClickListener(this)
+        binding.btnSignUp.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v) {
+            binding.btnLogin -> findNavController().navigate(R.id.action_landingFragment_to_logInFragment)
+            binding.btnSignUp -> findNavController().navigate(R.id.action_landingFragment_to_signUpFragment)
+        }
     }
 
     private fun initAdapter() {
