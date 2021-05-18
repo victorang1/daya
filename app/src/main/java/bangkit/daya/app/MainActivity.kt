53f97d7b7b5b3221e97f7bc3a4ac8b78501c6eea
victor <victor@binus.edu>
2021-05-18 19:37:52 +0700

    refactor code, replace login register edit text with material text field

--- app/src/main/java/bangkit/daya/app/MainActivity.kt
+++ app/src/main/java/bangkit/daya/app/MainActivity.kt
@@ -1,13 +1,19 @@
 package bangkit.daya.app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import bangkit.daya.R
 import bangkit.daya.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,5 +38,24 @@ class MainActivity : AppCompatActivity() {
             }
         }
         binding.dashboardBottomMenu.setupWithNavController(navController)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        if (currentFocus != null) {
+            val imm: InputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
+        if (ev.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    v.clearFocus()
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
